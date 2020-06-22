c60a33c3fcaad38f024a198c038b8123eac04cd4
brad <mybradtw@gmail.com>
2020-06-22 22:09:17 +0800

    HelloWorld

---
+++ src/com/bradchao/java/tutor/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bradchao.java.tutor;
+
+/*
+    這是HelloWorld程式
+ */
+public class HelloWorld {
+    // 程式執行進入點
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
